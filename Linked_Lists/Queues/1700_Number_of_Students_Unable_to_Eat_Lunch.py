3822ec4ccc7e7300332772da3d31e4c848efbbf9
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-09-26 14:53:25 -0400

    Create 1700_Number_of_Students_Unable_to_Eat_Lunch.py

---
+++ Linked_Lists/Queues/1700_Number_of_Students_Unable_to_Eat_Lunch.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        """
+        :type students: List[int]
+        :type sandwiches: List[int]
+        :rtype: int
+        """
+        res = len(students)
+        cnt = Counter(students)
+
+        for s in sandwiches:
+            if cnt[s] > 0:
+                res -= 1
+                cnt[s] -= 1
+            else:
+                return res
+
+        return res
